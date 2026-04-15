504e558cb07448bd623df3441b5d2e56ee98a8c5
Larry <user@users-MacBook-Air.local>
2026-04-15 23:55:28 +0100

    Use logo.jpg for header and footer branding

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -5,7 +5,7 @@ interface BrandLogoProps {
 export default function BrandLogo({ className = "" }: BrandLogoProps) {
   return (
     <img
-      src="/images/IMG_4741.jpg"
+      src="/images/logo.jpg"
       alt="Logo Mamouda Ndam Biofil"
       className={`h-11 w-11 rounded-full object-cover border-2 border-primary/20 shadow-sm ${className}`}
     />
